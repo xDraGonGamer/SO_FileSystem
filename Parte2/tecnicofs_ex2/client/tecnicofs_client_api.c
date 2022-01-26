4ce55f5ffda61d05a7570fe92b9e1e6f1da3730a
jmseca <joao.m.fonseca@gmail.com>
2022-01-26 19:30:11 +0000

    Finished beta of tecnicofs_client_api

--- Parte2/tecnicofs_ex2/client/tecnicofs_client_api.c
+++ Parte2/tecnicofs_ex2/client/tecnicofs_client_api.c
@@ -6,88 +6,131 @@
 #include <unistd.h>
 
 int fclient, fserver, session_id;
-char* cpath, spath;
+char* cpath;
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     char buffer[41];
-    buffer[0] = '0' + TFS_OP_CODE_MOUNT;
+    buffer[0] = (char) TFS_OP_CODE_MOUNT;
     buffer[1] = '\0';
-    strcopy(cpath, client_pipe_path);
-    strcopy(spath, server_pipe_path);
+    strcpy(cpath, client_pipe_path);
+    if(mkfifo(client_pipe_path, 0777) < 0){
+        return -1;
+    }
     if ((fserver = open(server_pipe_path, O_WRONLY)) < 0) return -1; //abre se o pipe de entrada de comunicacao do servidor
     if ((fclient = open(client_pipe_path, O_RDONLY)) < 0) return -1; //abre se o pipe de entrada de comunicacao do cliente
     strcat(buffer, client_pipe_path);
-    write(fserver, buffer, 41);
-    read(fclient, &session_id, sizeof(int));
+    if (write(fserver, buffer, 41) < 0)
+        return -1;
+    if (read(fclient, &session_id, sizeof(int)) < 0)
+        return -1;
     if(session_id == -1){
         close(fclient);
         close(fserver);
+        unlink(client_pipe_path);
         return -1;
     }
     return 0;
 }
 
+
+
+
 int tfs_unmount() {
-    if ((fserver = open(spath, O_WRONLY)) < 0) return -1;
-    if ((fclient = open(cpath, O_RDONLY)) < 0) return -1;
     char buffer[5];
-    buffer[0] = '0' + TFS_OP_CODE_MOUNT;
+    int serverResponse;
+    buffer[0] = TFS_OP_CODE_MOUNT;
     buffer[1] = '\0';
     strncat(buffer, (void*)&session_id, sizeof(int));
-    write(fserver, buffer, 41);
-    close(fclient);
-    close(fserver);
-    return -1;
+    if (write(fserver, buffer, 41) < 0)
+        return -1;
+    if (read(fclient, &serverResponse, sizeof(int)) < 0)
+        return -1;
+    if (serverResponse>=0){
+        close(fclient);
+        close(fserver);
+        unlink(cpath);
+    }
+    return serverResponse;
 }
 
 int tfs_open(char const *name, int flags) {
-    if ((fserver = open(spath, O_WRONLY)) < 0) return -1;
-    if ((fclient = open(cpath, O_RDONLY)) < 0) return -1;
     char buffer[49];
-    buffer[0] = '0' + TFS_OP_CODE_OPEN;
+    int serverResponse;
+    buffer[0] = TFS_OP_CODE_OPEN;
     buffer[1] = '\0';
     strncat(buffer, (void*)&session_id, sizeof(int));
     strcat(buffer, name);
     strncat(buffer, (void*)&flags, sizeof(int));
-    write(fserver, buffer, 49);
-    close(fclient);
-    close(fserver);
-    return -1;
+    if (write(fserver, buffer, 49) < 0)
+        return -1;
+    if (read(fclient, &serverResponse, sizeof(int)) < 0)
+        return -1;
+    return serverResponse;
 }
 
 int tfs_close(int fhandle) {
-    if ((fserver = open(spath, O_WRONLY)) < 0) return -1;
-    if ((fclient = open(cpath, O_RDONLY)) < 0) return -1;
     char buffer[9];
-    buffer[0] = '0' + TFS_OP_CODE_CLOSE;
+    int serverResponse;
+    buffer[0] = TFS_OP_CODE_CLOSE;
     buffer[1] = '\0';
     strncat(buffer, (void*)&session_id, sizeof(int));
     strncat(buffer, (void*)&fhandle, sizeof(int));
-    write(fserver, buffer, 9);
-    close(fclient);
-    close(fserver);
-    return -1;
+    if (write(fserver, buffer, 9) < 0)
+        return -1;
+    if (read(fclient, &serverResponse, sizeof(int)) < 0)
+        return -1;
+    return serverResponse;
 }
 
 ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
-    /* TODO: Implement this */
-    return -1;
+    char wBuffer[1041];
+    ssize_t serverResponse;
+    wBuffer[0] = TFS_OP_CODE_WRITE;
+    wBuffer[1] = '\0';
+    strncat(wBuffer, (void*)&session_id, sizeof(int));
+    strncat(wBuffer, (void*)&fhandle, sizeof(int));
+    strncat(wBuffer, (void*)&len, sizeof(size_t));
+    strncat(wBuffer, (void*)buffer , len);
+    if (write(fserver, wBuffer, 1041) < 0)
+        return -1;
+    if (read(fclient, &serverResponse, sizeof(ssize_t)) < 0)
+        return -1;
+    return serverResponse;
 }
 
 ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
-    /* TODO: Implement this */
-    return -1;
+    char* myBuffer = (char*) buffer;
+    size_t readBytesFromServer = len+8;
+    char rBuffer[readBytesFromServer]; // read info from server
+    char wBuffer[17]; // write info to server  
+    ssize_t serverResponse;
+    wBuffer[0] = TFS_OP_CODE_READ;
+    wBuffer[1] = '\0';
+    strncat(wBuffer, (void*)&session_id, sizeof(int));
+    strncat(wBuffer, (void*)&fhandle, sizeof(int));
+    strncat(wBuffer, (void*)&len, sizeof(size_t));
+    if (write(fserver, wBuffer, 17) < 0)
+        return -1;
+    if (read(fclient, &serverResponse, sizeof(ssize_t)) < 0)
+        return -1;
+    memcpy(&serverResponse,rBuffer,sizeof(ssize_t));
+    if (serverResponse>=0){
+        memcpy(myBuffer,&rBuffer[8],len);
+    } else {
+        myBuffer[0] = '\0';
+    }
+    return serverResponse;
 }
 
 int tfs_shutdown_after_all_closed() {
-    if ((fserver = open(spath, O_WRONLY)) < 0) return -1;
-    if ((fclient = open(cpath, O_RDONLY)) < 0) return -1;
     char buffer[5];
-    buffer[0] = '0' + TFS_OP_CODE_SHUTDOWN_AFTER_ALL_CLOSED;
+    int serverResponse;
+    buffer[0] = TFS_OP_CODE_SHUTDOWN_AFTER_ALL_CLOSED;
     buffer[1] = '\0';
     strncat(buffer, (void*)&session_id, sizeof(int));
-    write(fserver, buffer, 5);
-    close(fclient);
-    close(fserver);
-    return -1;
+    if (write(fserver, buffer, 5) < 0)
+        return -1;
+    if (read(fclient, &session_id, sizeof(int)) < 0)
+        return -1;
+    return serverResponse;
 }
--- Parte2/tecnicofs_ex2/fs/tfs_server.c
+++ Parte2/tecnicofs_ex2/fs/tfs_server.c
@@ -1,11 +1,15 @@
 #include "operations.h"
+#include <fcntl.h>
+
+#define INPUT_BUFFER_SIZE 1041 //buffer for info from client
 
 int clientCount = 0;
 
 int main(int argc, char **argv) {
-
+    char buffer[INPUT_BUFFER_SIZE];
     int clientCount = 0;
-
+    ssize_t ret;
+    char opCode;
     if (argc < 2) {
         printf("Please specify the pathname of the server's pipe.\n");
         return 1;
@@ -14,21 +18,62 @@ int main(int argc, char **argv) {
     char *pipename = argv[1];
     printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
-    if(mkfifo(pipename) < 0){
+    if(mkfifo(pipename,0777) < 0){
         exit(1);
     }
 
-    /* TO DO */
+    int fserver = open(pipename, O_RDONLY);
+    while (1){
+        ret = read(fserver, buffer, INPUT_BUFFER_SIZE);
+        if (!ret){ //TODO, just here for debuging (maybe)
+            fprintf(stderr, "[INFO]: pipe closed\n");
+            return 0;
+        } else if (ret == -1) { //TODO, just here for debuging (maybe)
+            fprintf(stderr, "[ERR]: read failed\n");
+            exit(EXIT_FAILURE);
+        } else {
+            memcpy(&opCode,buffer,sizeof(char));
+            switch (opCode)
+            {
+            case 1:
+                /* code */
+                break;
+            case 2:
+                /* code */
+                break;
+            case 3:
+                /* code */
+                break;
+            case 4:
+                /* code */
+                break;
+            case 5:
+                /* code */
+                break;
+            case 6:
+                /* code */
+                break;
+            case 7:
+                /* code */
+                break;
+            default:
+                fprintf(stderr, "[ERR]: switch X(\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+
+    }
 
     return 0;
 }
 
 
-int tfs_server_mount(char* joao){
+/*int tfs_server_mount(char* joao){
 
     if(clientCount < S){
         
     }
     return -1;
 
-}
+} TODO check if needed*/ 
