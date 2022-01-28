7226d4cab2b32dc805719a49e0195a9f99981059
jmseca <joao.m.fonseca@gmail.com>
2022-01-28 00:19:59 +0000

    make with no errors or warnings

--- Parte2/tecnicofs_ex2/fs/tfs_server.c
+++ Parte2/tecnicofs_ex2/fs/tfs_server.c
@@ -1,5 +1,6 @@
 #include "operations.h"
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #define INPUT_BUFFER_SIZE 1041 //buffer for info from client
@@ -21,14 +22,13 @@ int getClientFhandle(int sessionId){
     }
 }
 
-int openClientSession(char* client_pipe_name){
+int openClientSession(char* client_pipe_name, int* fhandle){
     // maybe lock? TODO
-    int fhandle;
     for (int i=0; i<S; i++){
         if (clientsFHandle[i]<0){
-            fhandle = open(client_pipe_name,O_WRONLY) < 0)
-            if (fhandle >= 0){
-                clientsFHandle[i] = fhandle;
+            *fhandle = open(client_pipe_name,O_WRONLY);
+            if (*fhandle >= 0){
+                clientsFHandle[i] = *fhandle;
                 return i;
             } else {
                 return -1;
@@ -49,18 +49,20 @@ int finishClientSession(int sessionId){
 }
 
 
-void handle_tfs_mount(char *bufferIn){
+ssize_t handle_tfs_mount(char *bufferIn){
     char bufferOut[sizeof(int)];
     char client_pipe_name[40];
-    int fclient,out;
+    int out, fclient = -1;
     strcpy(client_pipe_name,bufferIn);
-    out = openClientSession(client_pipe_name);
-    
+    out = openClientSession(client_pipe_name,&fclient);
+    if (fclient<0){
+        return -1; 
+    }
     memcpy(bufferOut,&out,sizeof(int));
-    write(fclient,bufferOut,sizeof(int));
+    return write(fclient,bufferOut,sizeof(int));
 }
 
-void handle_tfs_unmount(char *bufferIn){
+ssize_t handle_tfs_unmount(char *bufferIn){
     char bufferOut[sizeof(int)];
     int clientSessionID, fclient, out=-1;
     memcpy(&clientSessionID,bufferIn,sizeof(int));
@@ -71,10 +73,10 @@ void handle_tfs_unmount(char *bufferIn){
     }
     // TODO: Falta meter o clientsFHandle[SessionID] a -1, para saber que ha erro
     memcpy(bufferOut,&out,sizeof(int));
-    write(fclient,bufferOut,sizeof(int));
+    return write(fclient,bufferOut,sizeof(int));
 }
 
-void handle_tfs_open(char* bufferIn){
+ssize_t handle_tfs_open(char* bufferIn){
     char bufferOut[sizeof(int)];
     char fileName[40];
     int clientSessionID, fclient, flags, out;
@@ -85,10 +87,10 @@ void handle_tfs_open(char* bufferIn){
     out = tfs_open(fileName,flags);
 
     memcpy(bufferOut,&out,sizeof(int));
-    write(fclient,bufferOut,sizeof(int));
+    return write(fclient,bufferOut,sizeof(int));
 }
 
-void hanlde_tfs_close(char* bufferIn){
+ssize_t handle_tfs_close(char* bufferIn){
     char bufferOut[sizeof(int)];
     int clientSessionID, fclient, fhandle, out;
     memcpy(&clientSessionID,bufferIn,sizeof(int));
@@ -97,10 +99,10 @@ void hanlde_tfs_close(char* bufferIn){
     out = tfs_close(fhandle);
 
     memcpy(bufferOut,&out,sizeof(int));
-    write(fclient,bufferOut,sizeof(int));
+    return write(fclient,bufferOut,sizeof(int));
 }
 
-void hanlde_tfs_write(char* bufferIn){
+ssize_t handle_tfs_write(char* bufferIn){
     char bufferOut[sizeof(ssize_t)];
     char* toWrite;
     int clientSessionID, fclient, fhandle;
@@ -115,10 +117,10 @@ void hanlde_tfs_write(char* bufferIn){
     out = tfs_write(fhandle,toWrite,len);
     free(toWrite);    
     memcpy(bufferOut,&out,sizeof(ssize_t));
-    write(fclient,bufferOut,sizeof(ssize_t));
+    return write(fclient,bufferOut,sizeof(ssize_t));
 }
 
-void hanlde_tfs_write(char* bufferIn){
+ssize_t handle_tfs_read(char* bufferIn){
     char bufferOut[(sizeof(ssize_t)+BLOCK_SIZE)];
     int clientSessionID, fclient, fhandle;
     size_t len;
@@ -130,19 +132,19 @@ void hanlde_tfs_write(char* bufferIn){
     out = tfs_read(fhandle,&bufferOut[sizeof(ssize_t)],len);
 
     memcpy(bufferOut,&out,sizeof(ssize_t));
-    write(fclient,bufferOut,sizeof(ssize_t)+BLOCK_SIZE);
+    return write(fclient,bufferOut,sizeof(ssize_t)+BLOCK_SIZE);
 }
 
 
-void handle_tfs_shutdown_after_all_closed(char* bufferIn){
+ssize_t handle_tfs_shutdown_after_all_closed(char* bufferIn){
     char bufferOut[sizeof(int)];
     int clientSessionID, fclient, out;
     memcpy(&clientSessionID,bufferIn,sizeof(int));
     fclient = clientsFHandle[clientSessionID];
     out = tfs_destroy_after_all_closed();
 
-    memcpy(bufferOut,&out,sizeof(ssize_t));
-    write(fclient,bufferOut,sizeof(ssize_t)+BLOCK_SIZE);
+    memcpy(bufferOut,&out,sizeof(int));
+    return write(fclient,bufferOut,sizeof(int));
 }
 
 
@@ -151,8 +153,7 @@ void handle_tfs_shutdown_after_all_closed(char* bufferIn){
 
 int main(int argc, char **argv) {
     char bufferIn[INPUT_BUFFER_SIZE];
-    int clientCount = 0;
-    ssize_t ret;
+    ssize_t readOut,handleOut;
     char opCode;
     if (argc < 2) {
         printf("Please specify the pathname of the server's pipe.\n");
@@ -168,11 +169,11 @@ int main(int argc, char **argv) {
 
     int fserver = open(pipename, O_RDONLY);
     while (1){
-        ret = read(fserver, bufferIn, INPUT_BUFFER_SIZE);
-        if (!ret){ //TODO, just here for debuging (maybe)
+        readOut = read(fserver, bufferIn, INPUT_BUFFER_SIZE);
+        if (!readOut){ //TODO, just here for debuging (maybe)
             fprintf(stderr, "[INFO]: pipe closed\n");
             return 0;
-        } else if (ret == -1) { //TODO, just here for debuging (maybe)
+        } else if (readOut == -1) { //TODO, just here for debuging (maybe)
             fprintf(stderr, "[ERR]: read failed\n");
             exit(EXIT_FAILURE);
         } else {
@@ -180,29 +181,34 @@ int main(int argc, char **argv) {
             switch (opCode)
             {
             case 1:
-                handle_tfs_mount(&bufferIn[1]);
+                handleOut = handle_tfs_mount(&bufferIn[1]);
                 break;
             case 2:
-                handle_tfs_unmount(&bufferIn[1]);
+                handleOut = handle_tfs_unmount(&bufferIn[1]);
                 break;
             case 3:
-                handle_tfs_open(&bufferIn[1]);
+                handleOut = handle_tfs_open(&bufferIn[1]);
                 break;
             case 4:
-                handle_tfs_close(&bufferIn[1]);
+                handleOut = handle_tfs_close(&bufferIn[1]);
                 break;
             case 5:
-                handle_tfs_write(&bufferIn[1]);
+                handleOut = handle_tfs_write(&bufferIn[1]);
                 break;
             case 6:
-                handle_tfs_read(&bufferIn[1]);
+                handleOut = handle_tfs_read(&bufferIn[1]);
                 break;
             case 7:
-                handle_tfs_shutdown_after_all_closed(&bufferIn[1]);
+                handleOut = handle_tfs_shutdown_after_all_closed(&bufferIn[1]);
                 break;
             default:
                 fprintf(stderr, "[ERR]: switch X(\n");
                 exit(EXIT_FAILURE);
+            }
+
+            if (handleOut < 0){
+                fprintf(stderr, "[ERR]: switch X(\n");
+                exit(EXIT_FAILURE); 
             }
         }
 
